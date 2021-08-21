ce5ce13a949410ce087ba69b9b72207cbc2e448d
maitehilario <mhilariobolani@gmail.com>
2021-08-21 14:58:39 -0300

    login

--- js/login.js
+++ js/login.js
@@ -1,6 +1,29 @@
-//Función que se ejecuta una vez que se haya lanzado el evento de
+﻿//Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
+    document.getElementById("submitBtn").addEventListener("click", function(){
+       
+        let inputEmail = document.getElementById("inputEmail");
+        let inputPassword = document.getElementById("inputPassword");
+        let camposcompletos = true
 
+        if (inputEmail.value === ''){
+            inputEmail.classList.add("invalid");
+            camposcompletos = false;
+        } else {
+            inputEmail.classList.remove("invalid");
+        }
+
+        if (inputPassword.value === ''){
+            inputPassword.classList.add("invalid")
+            camposcompletos = false;
+        } else {
+            inputPassword.classList.remove("invalid");
+        }
+
+        if (camposcompletos){
+            window.location = 'inicio.html';
+        } else {alert("Debes ingresar los datos!") }
+    })
 });
